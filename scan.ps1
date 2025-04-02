13f3435232523eaf285b8481ff94b580c4ac0b76
mattmatheusmatt <93887538+mattmatheusmatt@users.noreply.github.com>
2025-04-02 15:26:28 -0300

    Enviando primeira versão do script

---
+++ scan.ps1
@@ -0,0 +1,12 @@
+﻿$ip = 192.168.56.101
+$portas = $(80,22)
+
+foreach($porta in $portas){
+    $teste = Test-NetConnection 192.168.56.101 -Port $porta 2>$null
+
+    if ($teste.TcpTestSucceeded){
+        Write-Host "Porta:" $porta "Aberta"
+    }
+
+    
+}
